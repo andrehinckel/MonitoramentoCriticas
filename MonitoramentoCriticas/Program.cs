df24a6db6b2062b1bc87cc0c47422c83ad485322
André Marcos Hinckel <andrehinckel@gmail.com>
2020-01-29 16:22:13 -0300

    Initial Commit

---
+++ MonitoramentoCriticas/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MonitoramentoCriticas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
